75a26ca4c289913cabdd793fff496f1cf8ed5430
sparta <manuel.sparta@gmail.com>
2015-11-09 22:42:06 +0100

    comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function save a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function check if the invertion of a matrix was computed
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
